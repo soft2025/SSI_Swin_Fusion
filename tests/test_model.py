2c8c6654442c4f54fea7ebf102511054641533fb
soft2025 <softwesternmtl@gmail.com>
2025-08-02 12:49:08 -0400

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,9 +2,9 @@ import torch
 from src.models import SSI_SwinFusionNet
 
 def test_forward_shape():
-    model = SSI_SwinFusionNet(num_classes=4, ssi_input_dim=64, pretrained=False)
+    model = SSI_SwinFusionNet(num_classes=4, ssi_input_dim=10, pretrained=False)
     imgs = torch.rand(2, 3, 224, 224)
-    ssis = torch.rand(2, 64)
+    ssis = torch.rand(2, 10)
     out = model(imgs, ssis)
     assert out.shape == (2, 4)
 
