7339291963c5db47787479fc18e2c23507d12299
soft2025 <softwesternmtl@gmail.com>
2025-08-02 12:30:57 -0400

    Update test_model_fusion.py

--- test_model_fusion.py
+++ test_model_fusion.py
@@ -3,5 +3,5 @@ from src.models.swin import SSI_SwinFusionNet
 
 model = SSI_SwinFusionNet(num_classes=4, ssi_input_dim=10, pretrained=False)
 img = torch.randn(2, 3, 224, 224)
-ssi = torch.randn(2, 64)
+ssi = torch.randn(2, 10)
 print("Output shape:", model(img, ssi).shape)
