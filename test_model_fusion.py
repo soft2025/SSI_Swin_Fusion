30dd65f6e32a5979ed092641400bf242e6c19663
soft2025 <softwesternmtl@gmail.com>
2025-08-03 10:45:09 -0400

    Rename swin model to ssi_swin and update imports

--- test_model_fusion.py
+++ test_model_fusion.py
@@ -1,5 +1,5 @@
 import torch
-from src.models.swin import SSI_SwinFusionNet
+from src.models.ssi_swin import SSI_SwinFusionNet
 
 model = SSI_SwinFusionNet(num_classes=4, ssi_input_dim=10, pretrained=False)
 img = torch.randn(2, 3, 224, 224)
